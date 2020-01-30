3dced0cd7c19268650f836d246224f4407e272c4
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-01-30 17:04:44 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://coronel-app-xml.herokuapp.com/rss.php");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("track");
+?>
+
+ <h1>Tracks</h1>
+
+ <?php
+ foreach( $content as $data )
+ {?>
+     <div class="border">
+     <?php
+     $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
+     $artist = $data->getElementsByTagName("artist")->item(0)->nodeValue;
+     $genre = $data->getElementsByTagName("genre")->item(0)->nodeValue;
+     $album = $data->getElementsByTagName("album")->item(0)->nodeValue;
+     echo "<ul>
+            <h2>$title</h2>
+              <ul>
+                  <li>Artist: $artist </li>
+                  <li>Genre: $genre </li>
+                  <li>Album: $album </li>
+              </ul>
+          </ul>";
+    ?>
+     </div>
+  <?php
+ }
+?>
+</div>
+</div>
